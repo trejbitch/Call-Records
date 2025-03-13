df27d636eb2c9bff958ff109c55c439571efd5b2
trejbitch <trejbalik77@gmail.com>
2025-03-13 15:07:26 +0000

    Update call-details.tsx

--- call-details.tsx
+++ call-details.tsx
@@ -4,7 +4,6 @@ src/components/ui/call-details.tsx
 
 
 
-
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { Textarea } from "@/components/ui/textarea"
